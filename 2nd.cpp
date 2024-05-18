981dfff8fc26cb55a995ebcf259a07746ff5c5c4
imlinux68 <imlinux68@gmail.com>
2024-05-18 23:55:34 +0300

    added second prog

---
+++ 2nd.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+
+  int favorite_num;
+  std::cout << "Enter your favorite num between 1 and 100: ";
+
+  std:cin >> favorite_num;
+  std::cout << "Amazing its my favorite num too" << std::endl;
+  return 0;
+
+
+}
---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM scratch
+
+COPY main.exe /
+
+CMD ["./main.exe"]
